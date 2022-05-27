f22f71e10e135510339a4ca5e34e43510ca85bca
oluwatayo <oluwatayoadegboye@gmail.com>
2022-05-27 15:43:20 +0100

    remove cash and cash advance transactions for wema cashout

--- app/src/main/java/com/woleapp/netpos/ui/fragments/TransactionsFragment.kt
+++ app/src/main/java/com/woleapp/netpos/ui/fragments/TransactionsFragment.kt
@@ -161,15 +161,26 @@ class TransactionsFragment : BaseFragment() {
                 addFragmentWithoutRemove(fragment)
             }
         }
-        val listOfService = arrayListOf<Service>(
-            Service(0, "Purchase", R.drawable.ic_purchase),
-            Service(1, "Cash", R.drawable.ic_baseline_money_24),
-            Service(2, "PRE AUTHORIZATION", R.drawable.ic_pre_auth),
-            Service(3, "Cash Advance", R.drawable.ic_pay_cash_icon),
-            Service(4, "QR", R.drawable.ic_qr_code),
-            Service(5, "Reprint", R.drawable.ic_print),
-            Service(6, "VEND", R.drawable.ic_vend)
-        )
+        val listOfService = if (BuildConfig.FLAVOR.equals("wemacashout", true)) {
+            arrayListOf(
+                Service(0, "Cash Out", R.drawable.ic_purchase),
+                Service(2, "PRE AUTHORIZATION", R.drawable.ic_pre_auth),
+                Service(4, "QR", R.drawable.ic_qr_code),
+                Service(5, "Reprint", R.drawable.ic_print),
+                Service(6, "VEND", R.drawable.ic_vend)
+            )
+        } else {
+            arrayListOf(
+                Service(0, "Purchase", R.drawable.ic_purchase),
+                Service(1, "Cash", R.drawable.ic_baseline_money_24),
+                Service(2, "PRE AUTHORIZATION", R.drawable.ic_pre_auth),
+                Service(3, "Cash Advance", R.drawable.ic_pay_cash_icon),
+                Service(4, "QR", R.drawable.ic_qr_code),
+                Service(5, "Reprint", R.drawable.ic_print),
+                Service(6, "VEND", R.drawable.ic_vend)
+            )
+        }
+
         adapter.submitList(listOfService)
     }
 }
