d3f3f58cf79d4602684c142e11ef99742444b263
Akozi Emmanuel <akoziemmanuel@gmail.com>
2022-10-07 11:24:07 +0100

    Increased database version

--- app/src/main/java/com/woleapp/netpos/database/AppDatabase.kt
+++ app/src/main/java/com/woleapp/netpos/database/AppDatabase.kt
@@ -18,7 +18,7 @@ import com.woleapp.netpos.util.RoomTypeConverters
 
 @Database(
     entities = [TransactionResponse::class, MqttEventsLocal::class, TransactionResponseXForTracking::class, GetZenithPayByTransferUserTransactionsModel::class],
-    version = 12,
+    version = 13,
     exportSchema = false
 )
 @TypeConverters(
