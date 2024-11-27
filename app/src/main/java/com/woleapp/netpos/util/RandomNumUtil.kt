a98afb8b7d95548fa390cc6fb1eb4dc83f85c315
Akozi Emmanuel <akoziemmanuel@gmail.com>
2024-11-27 13:31:49 +0100

    wemampgs

--- app/src/main/java/com/woleapp/netpos/util/RandomNumUtil.kt
+++ app/src/main/java/com/woleapp/netpos/util/RandomNumUtil.kt
@@ -44,7 +44,6 @@ import java.time.format.DateTimeFormatter
 import java.util.*
 
 object RandomNumUtil {
-
     @SuppressLint("SimpleDateFormat")
     fun getDateInMillis(dateTime: String): Long {
         val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.000Z'")
@@ -62,18 +61,14 @@ object RandomNumUtil {
     }
 
     @SuppressLint("SimpleDateFormat")
-    fun getDateInTheFormatExpectedByTheNewService(
-        date: String
-    ): String {
+    fun getDateInTheFormatExpectedByTheNewService(date: String): String {
         val initDate = SimpleDateFormat("dd:MM:yyyy hh:mm:ss").parse(date) ?: Date()
         val formatter = SimpleDateFormat("yyyy-MM-dd")
         return formatter.format(initDate) + " 00:00:00"
     }
 
     @SuppressLint("SimpleDateFormat")
-    fun getDateInTheFormatExpectedByTheNewServiceForEnd(
-        date: String
-    ): String {
+    fun getDateInTheFormatExpectedByTheNewServiceForEnd(date: String): String {
         val initDate = SimpleDateFormat("dd:MM:yyyy hh:mm:ss").parse(date) ?: Date()
         val formatter = SimpleDateFormat("yyyy-MM-dd")
         return formatter.format(initDate) + " 23:59:59"
@@ -97,8 +92,9 @@ object RandomNumUtil {
     fun dateStr2Long(dateStr: String): Long {
         return try {
             val c = Calendar.getInstance()
-            c.time = SimpleDateFormat("yyyy-MM-dd hh:mm")
-                .parse(dateStr)!!
+            c.time =
+                SimpleDateFormat("yyyy-MM-dd hh:mm")
+                    .parse(dateStr)!!
             c.timeInMillis
         } catch (e: ParseException) {
             e.printStackTrace()
@@ -107,12 +103,16 @@ object RandomNumUtil {
     }
 
     @SuppressLint("SimpleDateFormat")
-    fun dateStrToLong(dateStr: String, inputDateFormat: String): Long {
+    fun dateStrToLong(
+        dateStr: String,
+        inputDateFormat: String,
+    ): Long {
         val formattedDateString = dateStr.removeSuffix(dateStr.takeLast(3))
         return try {
             val c = Calendar.getInstance()
-            c.time = SimpleDateFormat(inputDateFormat)
-                .parse(dateStr)!!
+            c.time =
+                SimpleDateFormat(inputDateFormat)
+                    .parse(dateStr)!!
             c.timeInMillis
         } catch (e: ParseException) {
             e.printStackTrace()
@@ -205,7 +205,6 @@ object RandomNumUtil {
         ).format(System.currentTimeMillis())
             .format(Date())
 
-
     @SuppressLint("SimpleDateFormat")
     fun getCurrentDate(): String {
         val dateFormatter: DateFormat = SimpleDateFormat("yyyy-MM-dd")
@@ -215,12 +214,12 @@ object RandomNumUtil {
 
     fun mapTransactionResponse(transResp: TransactionResponse) =
         "CardHolder: ${transResp.cardHolder} " +
-                "\nCardType: ${transResp.cardLabel}" +
-                "\nAmount: ${transResp.amount} " +
-                "\nRRN: ${transResp.RRN} \n" +
-                "ResponseCode: ${transResp.responseCode} \n" +
-                "TransmissionDateTime: ${transResp.transmissionDateTime} \n" +
-                "Time in Millis: ${transResp.transactionTimeInMillis}"
+            "\nCardType: ${transResp.cardLabel}" +
+            "\nAmount: ${transResp.amount} " +
+            "\nRRN: ${transResp.RRN} \n" +
+            "ResponseCode: ${transResp.responseCode} \n" +
+            "TransmissionDateTime: ${transResp.transmissionDateTime} \n" +
+            "Time in Millis: ${transResp.transactionTimeInMillis}"
 
     @SuppressLint("SimpleDateFormat")
     fun getDate(milliSeconds: Long): String {
@@ -232,7 +231,10 @@ object RandomNumUtil {
     }
 
     @SuppressLint("SimpleDateFormat")
-    fun convertDateToStringFromMillis(milliSeconds: Long, format: String = "yyyy-MM-dd"): String {
+    fun convertDateToStringFromMillis(
+        milliSeconds: Long,
+        format: String = "yyyy-MM-dd",
+    ): String {
         val formatter = SimpleDateFormat(format)
 
         val calendar = Calendar.getInstance()
@@ -271,7 +273,7 @@ object RandomNumUtil {
                 transactionTimeInMillis = transactionTimeInMillis,
                 transactionType = transactionType.name,
                 transmissionDateTime = getCurrentDateTime(),
-                agentName = Singletons.getCurrentlyLoggedInUser()?.email!!
+                agentName = Singletons.getCurrentlyLoggedInUser()?.email!!,
             )
         }
     }
@@ -283,44 +285,43 @@ object RandomNumUtil {
         requestData: TransactionRequestData,
         customerName: String,
         terminal_id: String,
-        partnerId: String
-    ) =
-        this.cardData.expiryDate.let {
-            TransactionToLogBeforeConnectingToNibbs(
-                status = "PENDING",
-                TransactionResponseX(
-                    AID = "",
-                    rrn = getCustomRrn(),
-                    STAN = generateRandomRrn(6),
-                    TSI = "",
-                    TVR = "",
-                    accountType = accountType.name,
-                    acquiringInstCode = "",
-                    additionalAmount_54 = "",
-                    amount = amount.toInt(),
-                    appCryptogram = "",
-                    authCode = "",
-                    cardExpiry = it,
-                    cardHolder = customerName,
-                    cardLabel = cardScheme.toString(),
-                    id = 0,
-                    localDate_13 = getDate(),
-                    localTime_12 = formattedTime.replace(":", ""),
-                    maskedPan = cardData.pan,
-                    merchantId = partnerId,
-                    originalForwardingInstCode = "",
-                    otherAmount = requestData.otherAmount.toInt(),
-                    otherId = "",
-                    responseCode = "99",
-                    responseDE55 = "",
-                    terminalId = terminal_id,
-                    transactionTimeInMillis = 0,
-                    transactionType = requestData.transactionType.name,
-                    transmissionDateTime = getCurrentDateTime(),
-                    agentName = Singletons.getCurrentlyLoggedInUser()?.email!!
-                )
-            )
-        }
+        partnerId: String,
+    ) = this.cardData.expiryDate.let {
+        TransactionToLogBeforeConnectingToNibbs(
+            status = "PENDING",
+            TransactionResponseX(
+                AID = "",
+                rrn = getCustomRrn(),
+                STAN = generateRandomRrn(6),
+                TSI = "",
+                TVR = "",
+                accountType = accountType.name,
+                acquiringInstCode = "",
+                additionalAmount_54 = "",
+                amount = amount.toInt(),
+                appCryptogram = "",
+                authCode = "",
+                cardExpiry = it,
+                cardHolder = customerName,
+                cardLabel = cardScheme.toString(),
+                id = 0,
+                localDate_13 = getDate(),
+                localTime_12 = formattedTime.replace(":", ""),
+                maskedPan = cardData.pan,
+                merchantId = partnerId,
+                originalForwardingInstCode = "",
+                otherAmount = requestData.otherAmount.toInt(),
+                otherId = "",
+                responseCode = "99",
+                responseDE55 = "",
+                terminalId = terminal_id,
+                transactionTimeInMillis = 0,
+                transactionType = requestData.transactionType.name,
+                transmissionDateTime = getCurrentDateTime(),
+                agentName = Singletons.getCurrentlyLoggedInUser()?.email!!,
+            ),
+        )
+    }
 
     fun setField59(aid: String): String {
         val additionalTagManipulation =
@@ -332,50 +333,55 @@ object RandomNumUtil {
     }
 
     fun getDeviceId(context: Context): String {
-        val deviceId: String = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-        } else {
-            val mTelephony = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-            if (mTelephony.deviceId != null) {
-                mTelephony.deviceId
+        val deviceId: String =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
             } else {
-                Settings.Secure.getString(
-                    context.contentResolver,
-                    Settings.Secure.ANDROID_ID,
-                )
+                val mTelephony = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+                if (mTelephony.deviceId != null) {
+                    mTelephony.deviceId
+                } else {
+                    Settings.Secure.getString(
+                        context.contentResolver,
+                        Settings.Secure.ANDROID_ID,
+                    )
+                }
             }
-        }
         return deviceId
     }
 
-    private val bankList = mapOf(
-        "konga" to "konga",
-        "easypay" to "fcmb",
-        "wemacashout" to "wemacashout",
-        "tingopay" to "tingopay",
-        "providus" to "providus",
-        "heritage" to "heritage",
-        "wema" to "wemabank",
-        "zenith" to "zenith",
-        "unitybank" to "unitybank",
-        "aellacredit" to "aellacredit",
-        "netpos" to "netpos",
-    )
+    private val bankList =
+        mapOf(
+            "konga" to "konga",
+            "easypay" to "fcmb",
+            "wemacashout" to "wemacashout",
+            "tingopay" to "tingopay",
+            "providus" to "providus",
+            "heritage" to "heritage",
+            "wema" to "wemabank",
+            "wemampgs" to "wemabank",
+            "zenith" to "zenith",
+            "unitybank" to "unitybank",
+            "aellacredit" to "aellacredit",
+            "netpos" to "netpos",
+        )
 
     fun getBankName(): String? = bankList[BuildConfig.FLAVOR]
 
     fun initPartnerId(): String {
         var partnerID = ""
-        val bankList = mapOf(
-            "firstbank" to "7FD43DF1-633F-4250-8C6F-B49DBB9650EA",
-            "easypay" to "1B0E68FD-7676-4F2C-883D-3931C3564190",
-            "stanbic" to "377F47E9-55F9-45E0-B77A-1BAA4BC88026",
-            "providus" to "8B26F328-040F-4F27-A5BC-4414AB9D1EFA",
-            "providus" to "8B26F328-040F-4F27-A5BC-4414AB9D1EFA",
-            "wema" to "1E3D050B-6995-495F-982A-0511114959C8",
-            "zenith" to "C936667C-0B02-4A34-80D0-0FC5B525256E",
-            "tingopay" to "1EED19E0-9625-49AA-A0CF-2EFCD8F30036",
-        )
+        val bankList =
+            mapOf(
+                "firstbank" to "7FD43DF1-633F-4250-8C6F-B49DBB9650EA",
+                "easypay" to "1B0E68FD-7676-4F2C-883D-3931C3564190",
+                "stanbic" to "377F47E9-55F9-45E0-B77A-1BAA4BC88026",
+                "providus" to "8B26F328-040F-4F27-A5BC-4414AB9D1EFA",
+                "providus" to "8B26F328-040F-4F27-A5BC-4414AB9D1EFA",
+                "wema" to "1E3D050B-6995-495F-982A-0511114959C8",
+                "wemampgs" to "1E3D050B-6995-495F-982A-0511114959C8",
+                "zenith" to "C936667C-0B02-4A34-80D0-0FC5B525256E",
+                "tingopay" to "1EED19E0-9625-49AA-A0CF-2EFCD8F30036",
+            )
 
         for (element in bankList) {
             if (element.key == BuildConfig.FLAVOR) {
@@ -387,17 +393,28 @@ object RandomNumUtil {
 
     fun displayCurrency(): ArrayList<String> {
         return arrayListOf(
-            "EUR", "USD", "NGN",
-            "RUB", "QAR", "ZAR",
-            "AED", "AUD", "CAD",
-            "CDF", "CHF", "CNY",
-            "MAD", "GBP", "GHS",
-            "GMD", "HKD", "JPY"
+            "EUR",
+            "USD",
+            "NGN",
+            "RUB",
+            "QAR",
+            "ZAR",
+            "AED",
+            "AUD",
+            "CAD",
+            "CDF",
+            "CHF",
+            "CNY",
+            "MAD",
+            "GBP",
+            "GHS",
+            "GMD",
+            "HKD",
+            "JPY",
         )
     }
 
     fun getNetPlusPayMid(): String = getCurrentlyLoggedInUser()?.netplusPayMid ?: STRING_MERCHANT_ID
-
 
     fun <T> observeServerResponseActivity(
         context: Context,
@@ -497,7 +514,7 @@ object RandomNumUtil {
         serverResponse: LiveData<Resource<T>?>,
         loadingDialog: AlertDialog,
         fragmentManager: FragmentManager,
-        successAction: () -> Unit
+        successAction: () -> Unit,
     ) {
         serverResponse.observe(this.viewLifecycleOwner) {
             when (it?.status) {
@@ -521,12 +538,10 @@ object RandomNumUtil {
         }
     }
 
-
-    fun alertDialog(
-        context: Context,
-    ): AlertDialog {
-        val dialogView: View = LayoutInflater.from(context)
-            .inflate(R.layout.layout_loading_dialog, null)
+    fun alertDialog(context: Context): AlertDialog {
+        val dialogView: View =
+            LayoutInflater.from(context)
+                .inflate(R.layout.layout_loading_dialog, null)
         val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(context)
         dialogBuilder.setCancelable(false)
         dialogBuilder.setView(dialogView)
@@ -536,7 +551,7 @@ object RandomNumUtil {
 
     fun alertDialog(
         context: Context,
-        setCancelable: Boolean
+        setCancelable: Boolean,
     ): AlertDialog {
         val dialogView: View =
             LayoutInflater.from(context).inflate(R.layout.layout_loading_dialog, null)
@@ -547,8 +562,10 @@ object RandomNumUtil {
         return dialogBuilder.create()
     }
 
-
-    fun LifecycleOwner.showSnackBar(rootView: View, message: String) {
+    fun LifecycleOwner.showSnackBar(
+        rootView: View,
+        message: String,
+    ) {
         Snackbar.make(rootView, message, Snackbar.LENGTH_LONG).show()
     }
 
@@ -556,6 +573,7 @@ object RandomNumUtil {
         val format = NumberFormat.getCurrencyInstance(Locale("en", "NG"))
         return format.format(amount)
     }
+
     fun Fragment.showToast(message: String) {
         Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
@@ -563,7 +581,10 @@ object RandomNumUtil {
     fun getGUID() = UUID.randomUUID().toString().replace("-", "")
 
     fun createClientDataForNonVerveCard(
-        transID: String, cardNumber: String, expiryDate: String, cvv: String
+        transID: String,
+        cardNumber: String,
+        expiryDate: String,
+        cvv: String,
     ): String = "$transID:LIVE:$cardNumber:$expiryDate:$cvv::NGN:QR"
 
     fun stringToBase64(text: String): String {
